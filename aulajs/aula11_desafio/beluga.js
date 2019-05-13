a2ba9d27fcafb33dfc25edf1ad7545dd3d62f162
George Salu <george.salu10@gmail.com>
2019-05-13 20:13:24 -0300

    resolução desafio

--- aulajs/aula11_desafio/beluga.js
+++ aulajs/aula11_desafio/beluga.js
@@ -1,13 +1,13 @@
-function calcularMedia(){                    
-                    
+function calcularMedia() {
+
     var total = 0;
     var qtd = arguments.length;
     var x = 0;
     //while(arguments[x]){
-    while(typeof arguments[x] === 'number'){
+    while (typeof arguments[x] === 'number') {
         total += arguments[x];
         x++
-    }                    
+    }
 
-    return total / qtd;
+    return (total / qtd).toFixed(2);
 }
